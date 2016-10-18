c1946f8caad23873f6b3c32df79ba3e30dbba92c
shunzi2 <shunzi@qq.com>
2016-10-18 10:49:34 +0800

    项目基本配置

---
+++ XMGFM/Classes/Other/System/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XMGFM
+//
+//  Created by 王顺子 on 16/10/18.
+//  Copyright © 2016年 小码哥. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
